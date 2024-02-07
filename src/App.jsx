2643c2c12f277ed7ffad6b6115d91b35ac2ce477
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-02-07 21:58:59 +0530

    added routing,components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import PrivateRoutes from './components/PrivateRoutes'
 import Room from './pages/Room'
+import LoginPage from './pages/LoginPage'
 
 function App() {
 
   return (
-    <>
-      <Room />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+
+        // So protecting the route, It will only pass it to child routes if user is authenticated
+        <Route element={<PrivateRoutes />}>
+          <Route path='/' element={<Room />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
