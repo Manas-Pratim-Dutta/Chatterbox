1b46c20af6ce14e5cde7ab8d0e4de05d40fb58bb
Manas Pratim Dutta <manaspratimdutta1999@gmail.com>
2024-02-08 21:33:55 +0530

    Added authContext,login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import PrivateRoutes from './components/PrivateRoutes'
+import { AuthProvider } from './utils/AuthContext'
+
 import Room from './pages/Room'
 import LoginPage from './pages/LoginPage'
 
@@ -8,15 +10,17 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
+      <AuthProvider>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
 
         // So protecting the route, It will only pass it to child routes if user is authenticated
-        <Route element={<PrivateRoutes />}>
-          <Route path='/' element={<Room />} />
-        </Route>
+          <Route element={<PrivateRoutes />}>
+            <Route path='/' element={<Room />} />
+          </Route>
 
-      </Routes>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth } from "../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = ()=> {
-    return(
-        <div>LoginPage</div>
+const LoginPage = () => {
+
+    const { user } = useAuth()
+    const navigate = useNavigate()
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    })
+
+    useEffect(() => {
+        if (user) {
+            navigate('/')
+        }
+    }, [])
+
+    const handleInputChange = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        setCredentials({ ...credentials, [name]: value })
+        console.log(credentials);
+    }
+
+    return (
+        <div className="auth--container">
+            <div className="form--wrapper">
+                <form>
+                    <div className="field-wrapper">
+                        <label>Email:</label>
+                        <input
+                            type="email"
+                            required
+                            name="email"
+                            placeholder="Enter Your Email..."
+                            value={credentials.email}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+
+                    <div className="field-wrapper">
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            required
+                            name="password"
+                            placeholder="Enter Your Password..."
+                            value={credentials.password}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="field-wrapper">
+                        <input className="btn btn--lg btn--main" type="submit" value="Login" />
+                    </div>
+                </form>
+
+            </div>
+
+        </div>
     )
 }
 
